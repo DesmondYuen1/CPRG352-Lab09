3a75b46b30502d02f00fb7d59ade6a307e67719e
user <user@DESKTOP-FLHNA44.cg.shawcable.net>
2021-11-14 20:35:33 -0700

    Everything is good but the update function.

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -31,15 +31,16 @@ public class UserServlet extends HttpServlet {
                 Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
         } else if (action != null && action.equals("delete")) {
-            String deletedUser = request.getParameter("deletedUser");
+            String deleteUser = request.getParameter("email");
             try {
-                us.delete(deletedUser);
+                us.delete(deleteUser);
             } catch (Exception ex) {
                 Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
-
+            response.sendRedirect("user");
+            return;
         }
-        
+
         try {
             List<User> users = us.getAll();
             request.setAttribute("users", users);
@@ -47,7 +48,6 @@ public class UserServlet extends HttpServlet {
             Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
         getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
-        return;
 
     }
 
@@ -58,100 +58,77 @@ public class UserServlet extends HttpServlet {
         UserService us = new UserService();
 
         String action = request.getParameter("action");
-        if (action != null) {
 
-            Boolean active = null;
-            int role = 0;
+        Boolean active = null;
+        int role = 0;
 
-            switch (action) {
-                case "add":
-                    try {
-                        String email = request.getParameter("email");
-                        String first_name = request.getParameter("first_name");
-                        String last_name = request.getParameter("last_name");
-                        String password = request.getParameter("password");
+        switch (action) {
+            case "add":
+                try {
+                    String email = request.getParameter("email");
+                    String first_name = request.getParameter("first_name");
+                    String last_name = request.getParameter("last_name");
+                    String password = request.getParameter("password");
 
-                        switch (request.getParameter("role")) {
-                            case "1":
-                                role = 1;
-                                break;
-                            case "2":
-                                role = 2;
-                                break;
-                            case "3":
-                                role = 3;
-                                break;
-                        }
-
-                        switch (request.getParameter("active[]")) {
-                            case "yes":
-                                active = true;
-                                break;
-                            case "no":
-                                active = false;
-                                break;
-                        }
-
-                        us.insert(email, active, first_name, last_name, password, role);
-
-                    } catch (Exception ex) {
-                        Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    switch (request.getParameter("role")) {
+                        case "1":
+                            role = 1;
+                            break;
+                        case "2":
+                            role = 2;
+                            break;
+                        case "3":
+                            role = 3;
+                            break;
                     }
 
-                    try {
-                        List<User> users = us.getAll();
-                        request.setAttribute("users", users);
-                    } catch (Exception ex) {
-                        Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
-                    return;
-
-                case "edit":
-                    try {
-                        String email = request.getParameter("selectedUser");
-                        User user = us.get(email);
-                        String first_name = request.getParameter("first_nameUp");
-                        String last_name = request.getParameter("last_nameUp");
-
-                        switch (request.getParameter("roleUp")) {
-                            case "1":
-                                role = 1;
-                                break;
-                            case "2":
-                                role = 2;
-                                break;
-                            case "3":
-                                role = 3;
-                                break;
-                        }
-
-                        switch (request.getParameter("activeUp[]")) {
-                            case "yesUp":
-                                active = true;
-                                break;
-                            case "noUp":
-                                active = false;
-                                break;
-                        }
-
-                        us.update(email, active, first_name, last_name, user.getPassword(), role);
-
-                    } catch (Exception ex) {
-                        Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    switch (request.getParameter("active[]")) {
+                        case "yes":
+                            active = true;
+                            break;
+                        case "no":
+                            active = false;
+                            break;
                     }
 
-                    try {
-                        List<User> users = us.getAll();
-                        request.setAttribute("users", users);
-                    } catch (Exception ex) {
-                        Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    response.sendRedirect("users");
-                    return;
-            }
+                    us.insert(email, active, first_name, last_name, password, role);
 
+                } catch (Exception ex) {
+                    Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                try {
+                    List<User> users = us.getAll();
+                    request.setAttribute("users", users);
+                } catch (Exception ex) {
+                    Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+                return;
+
+            case "update":
+                try {
+                    String email = request.getParameter("email");
+                    User user = us.get(email);
+                    String editFirstName = request.getParameter("editFirstName");
+                    String editLastName = request.getParameter("editLastName");
+                    int editRole = Integer.parseInt(request.getParameter("editRole"));
+
+                    switch (request.getParameter("activeUp[]")) {
+                        case "yesUp":
+                            active = true;
+                            break;
+                        case "noUp":
+                            active = false;
+                            break;
+                    }
+                    us.update(email, active, editFirstName, editLastName, user.getPassword(), editRole);
+
+                } catch (Exception ex) {
+                    Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+                }
         }
+
         try {
             List<User> users = us.getAll();
             request.setAttribute("users", users);
@@ -159,6 +136,5 @@ public class UserServlet extends HttpServlet {
             Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
         getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
-        return;
     }
 }
--- web/WEB-INF/users.jsp
+++ web/WEB-INF/users.jsp
@@ -30,7 +30,7 @@
     <body>
         <div id="addUser">
             <h1>Add User</h1>
-            <form action="" method="POST">
+            <form action="user" method="post">
                 <input type="email" name="email" placeholder="Email" value=""><br>
                 <input type="text" name="first_name" placeholder="First Name" value=""><br>
                 <input type="text" name="last_name" placeholder="Last Name" value=""><br>
@@ -70,8 +70,8 @@
                         <td>${user.email}</td>
                         <td>${user.firstName}</td>
                         <td>${user.lastName}</td>
-                        <td><a href="users?action=edit&email=${user.email}">Edit</a></td>
-                        <td><a href="users?action=delete&email=${user.email}">Delete</a></td>
+                        <td><a href="user?action=edit&amp;email=${user.email}">Edit</a></td>
+                        <td><a href="user?action=delete&amp;email=${user.email}">Delete</a></td>
                     </tr>
                 </c:forEach>
 
@@ -79,39 +79,44 @@
         </div>
 
         <div id="editUser">
+
             <h1>Edit User</h1>
 
-            <form action="user" method="POST">
-                <input type="text" name="first_nameUp" placeholder="First Name" value="${selectedUser.first_name}"><br>
-                <input type="text" name="last_nameUp" placeholder="Last Name" value="${selectedUser.last_name}"><br>
+            <form action="user" method="post">
+                <c:if test="${editUser != null}">
+                    <input type="hidden" name="editEmail" value="${editUser.email}"><br>
+                    <input type="text" name="editFirstName" value="${editUser.firstName}"><br>
+                    <input type="text" name="editLastName" value="${editUser.lastName}"><br>
+                    <input type="password" name="editPassword" value="${editUser.password}"><br>
 
+                    <select name="editRole">
+                        <option value="1"
+                                <c:if test="${editUser.role.roleId == 1}">selected</c:if>
+                                    >system admin</option>
+                                <option value="2"
+                                <c:if test="${editUser.role.roleId == 2}">selected</c:if>
+                                    >regular user</option>
+                                <option value="3"
+                                <c:if test="${editUser.role.roleId == 3}">selected</c:if>
+                                    >company admin</option>
+                        </select>
+                        <br>
 
-                <select name="roleUp">
-                    <option value="1"
-                            <c:if test="${selectedUser.role == 1}">selected</c:if>
-                                >system admin</option>
-                            <option value="2"
-                            <c:if test="${selectedUser.role == 2}">selected</c:if>
-                                >regular user</option>
-                            <option value="3"
-                            <c:if test="${selectedUser.role == 3}">selected</c:if>
-                                >company admin</option>
-                    </select>
-                    <br>
+                        Active:
+                        <input type="radio" id="yesUp" name="activeUp[]" value="yesUp"
+                        <c:if test="${editUser.active == true}">checked</c:if>
+                            >
+                        <label for="yesUp">Yes</label>
+                        <input type="radio" id="noUp" name="activeUp[]" value="noUp"
+                        <c:if test="${editUser.active == false}">checked</c:if>
+                            >
+                        <label for="noUp">No</label>
+                        <br>
 
-                    Active:
-                    <input type="radio" id="yesUp" name="activeUp[]" value="yesUp"
-                    <c:if test="${selectedUser.active == true}">checked</c:if>
-                        >
-                    <label for="yesUp">Yes</label>
-                    <input type="radio" id="noUp" name="activeUp[]" value="noUp"
-                    <c:if test="${selectedUser.active == false}">checked</c:if>
-                    >
-                <label for="noUp">No</label>
-                <br>
+                        <input type="hidden" name="action" value="update">
+                        <input type="submit" value="Update">
 
-                <input type="submit" value="Update">
-                <input type="hidden" name="action" value="update">
+                </c:if>
             </form>
 
         </div>
